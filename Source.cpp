4ab2d22e933e08e3f0e2d5e19dc182f0839f8a3b
ErikUchiha16 <35504246+ErikUchiha16@users.noreply.github.com>
2018-01-18 13:03:14 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,14 @@
-//write a program that prints a corny joke or pun to the console window :D
+#include<iostream>
+using namespace std;
+
+int main() {
+	int bob;
+	cout << "how many blocks wide" << endl;
+	cin >> bob;
+
+	for (int i = 0; i < bob; i++) {
+		for (int j = 0; j < bob; j++)
+			cout << '#';
+		cout << endl;
+	}
+}
